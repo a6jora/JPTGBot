546c6383dc456a5891b05b1fbe536b2706383fc3
Александр <45771682+a6jora@users.noreply.github.com>
2021-09-23 07:18:44 +0300

    Initial commit

---
+++ ChapterFourClass.py
@@ -0,0 +1,22 @@
+from ChapterClass import ChapterClass
+
+
+class ChapterFourClass(ChapterClass):
+
+    def __init__(self):
+        dictionary = {'': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': '',
+                      '': '', '': ''}
+        super().__init__(dictionary)
